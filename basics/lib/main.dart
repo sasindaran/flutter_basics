fc44300c84b07b5fe5ce5a338dc673b21008fd62
sasisam007 <sasisam007@gmail.com>
2023-09-20 23:10:45 +0530

    button to change content
    using a add button to change content hello world to hi friends

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -4,13 +4,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String s = "Hello world";
+
+  void clickme() {
+    setState(() {
+      s = "HI!!! Friends";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("HELLO PEOPLE!!!"),
+    return MaterialApp(
+      home: Column(
+        children: <Widget>[
+          Text(s),
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: clickme,
+          )
+        ],
+      ),
     );
   }
 }
